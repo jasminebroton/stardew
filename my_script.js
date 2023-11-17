982272edac7f01ab34cac7a9e79dd1baaffe0c9b
Jasmine Broton <jbroton@emich.edu>
2023-11-17 10:06:55 -0500

    adding my_script.js inital commit

---
+++ my_script.js
@@ -0,0 +1,152 @@
+var crops = {
+    "amaranth": {
+        "seeds": {
+            "pierre":70,
+            "joja":87
+        }, 
+        "growth":{
+            "inital": 7, 
+            "regrow": 0
+        }
+    },
+    "blue jazz": {
+        "seeds": {
+            "pierre":30,
+            "joja":37
+        },
+        "growth": {
+            "inital": 7,
+            "regrow":0
+        }
+    },
+    "cauliflower": {
+        "seeds": {
+            "pierre":80,
+            "joja":100
+        },
+        "growth": {
+            "inital": 12,
+            "regrow":0
+        }
+    },
+    "coffee bean": {
+        "seeds": {
+            "pierre":0,
+            "joja":0,
+        },
+        "growth": {
+            "inital": 10,
+            "regrow":2
+        }
+    },
+    "garlic": {
+        "seeds": {
+            "pierre":40,
+            "joja":0,
+        },
+        "growth": {
+            "inital": 4,
+            "regrow":0
+        }
+    },
+    "green bean": {
+        "seeds": {
+            "pierre":60,
+            "joja":75,
+        },
+        "growth": {
+            "inital": 10,
+            "regrow":3
+        }
+    },
+    "kale": {
+        "seeds": {
+            "pierre":70,
+            "joja":87,
+        },
+        "growth": {
+            "inital": 6,
+            "regrow":0
+        }
+    },
+    "parsnip": {
+        "seeds": {
+            "pierre":20,
+            "joja":25,
+        },
+        "growth": {
+            "inital": 4,
+            "regrow":0
+        }
+    },
+    "potato": {
+        "seeds": {
+            "pierre":50,
+            "joja":62,
+        },
+        "growth": {
+            "inital": 6,
+            "regrow":0
+        }
+    },
+    "rhubarb": {
+        "seeds": {
+            "pierre":0,
+            "joja":0,
+        },
+        "growth": {
+            "inital": 6,
+            "regrow":0
+        }
+    },
+    "strawberry": {
+        "seeds": {
+            "pierre":0,
+            "joja":0,
+        },
+        "growth": {
+            "inital": 8,
+            "regrow":4
+        }
+    },
+    "tulip": {
+        "seeds": {
+            "pierre":20,
+            "joja":25,
+        },
+        "growth": {
+            "inital": 6,
+            "regrow":0
+        }
+    },
+    "rice": {
+        "seeds": {
+            "pierre":40,
+            "joja":0,
+        },
+        "growth": {
+            "inital": 6,
+            "regrow":0
+        }
+    },
+    "blueberry": {
+        "seeds": {
+            "pierre":80,
+            "joja":0,
+        },
+        "growth": {
+            "inital": 13,
+            "regrow":4
+        }
+    },
+    "corn": {
+        "seeds": {
+            "pierre":150,
+            "joja":187,
+        },
+        "growth": {
+            "inital": 14,
+            "regrow":4
+        }
+    },
+}
