31cd9bc8b8a7f1442714463a3930b4661c40fab7
Jasmine Broton <jbroton@emich.edu>
2023-11-17 16:50:40 -0500

    commit 11/17

---
+++ data.js
@@ -0,0 +1,607 @@
+let crops = {
+    "amaranth": {
+        "seeds": {
+            "pierre":70,
+            "joja":87,
+            "special":0
+        }, 
+        "growth":{
+            "inital": 7, 
+            "regrow": 0
+        }
+    },
+    "blueJazz": {
+        "seeds": {
+            "pierre":30,
+            "joja":37,
+            "special":0
+        },
+        "growth": {
+            "inital": 7,
+            "regrow":0
+        },
+        "sells" : {
+            "base":50,
+            "silver": 62,
+            "gold": 75, 
+            "iridium":100
+        }
+    },
+    "cauliflower": {
+        "seeds": {
+            "pierre":80,
+            "joja":100,
+            "special":0
+        },
+        "growth": {
+            "inital": 12,
+            "regrow":0
+        },
+        "sells" : {
+            "base":175,
+            "silver": 218,
+            "gold": 262, 
+            "iridium":350
+        }
+    },
+    "coffeeBean": {
+        "seeds": {
+            "pierre":0,
+            "joja":0,
+            "special":2500
+        },
+        "growth": {
+            "inital": 10,
+            "regrow":2
+        },
+        "sells" : {
+            "base":15,
+            "silver": 18,
+            "gold": 22, 
+            "iridium":30
+        }
+    },
+    "garlic": {
+        "seeds": {
+            "pierre":40,
+            "joja":0,
+            "special":0
+        },
+        "growth": {
+            "inital": 4,
+            "regrow":0
+        },
+        "sells" : {
+            "base":60,
+            "silver": 75,
+            "gold": 90, 
+            "iridium":120
+        }
+    },
+    "greenBean": {
+        "seeds": {
+            "pierre":60,
+            "joja":75,
+            "special":0
+        },
+        "growth": {
+            "inital": 10,
+            "regrow":3
+        },
+        "sells" : {
+            "base":40,
+            "silver": 50,
+            "gold": 60, 
+            "iridium":80
+        }
+    },
+    "kale": {
+        "seeds": {
+            "pierre":70,
+            "joja":87,
+            "special":0
+        },
+        "growth": {
+            "inital": 6,
+            "regrow":0
+        },
+        "sells" : {
+            "base":110,
+            "silver": 137,
+            "gold": 165, 
+            "iridium":220
+        }
+    },
+    "parsnip": {
+        "seeds": {
+            "pierre":20,
+            "joja":25,
+            "special":0
+        },
+        "growth": {
+            "inital": 4,
+            "regrow":0
+        },
+        "sells" : {
+            "base":35,
+            "silver": 43,
+            "gold": 52, 
+            "iridium":70
+        }
+    },
+    "potato": {
+        "seeds": {
+            "pierre":50,
+            "joja":62,
+        },
+        "growth": {
+            "inital": 6,
+            "regrow":0
+        },
+        "sells" : {
+            "base":80,
+            "silver": 100,
+            "gold": 120, 
+            "iridium":160
+        }
+    },
+    "rhubarb": {
+        "seeds": {
+            "pierre":0,
+            "joja":0,
+            "special":100
+        },
+        "growth": {
+            "inital": 6,
+            "regrow":0
+        },
+        "sells" : {
+            "base":220,
+            "silver": 275,
+            "gold": 330, 
+            "iridium":440
+        }
+    },
+    "strawberry": {
+        "seeds": {
+            "pierre":0,
+            "joja":0,
+            "special":100
+        },
+        "growth": {
+            "inital": 8,
+            "regrow":4
+        },
+        "sells" : {
+            "base":120,
+            "silver": 150,
+            "gold": 180, 
+            "iridium":240
+        }
+    },
+    "tulip": {
+        "seeds": {
+            "pierre":20,
+            "joja":25,
+            "special":0
+        },
+        "growth": {
+            "inital": 6,
+            "regrow":0
+        },
+        "sells" : {
+            "base":30,
+            "silver": 37,
+            "gold": 45, 
+            "iridium":60
+        }
+    },
+    "rice": {
+        "seeds": {
+            "pierre":40,
+            "joja":0,
+            "special":0
+        },
+        "growth": {
+            "inital": 6,
+            "regrow":0
+        },
+        "sells" : {
+            "base":30,
+            "silver": 37,
+            "gold": 45, 
+            "iridium":60
+        }
+    },
+    "blueberry": {
+        "seeds": {
+            "pierre":80,
+            "joja":0,
+            "special":0
+        },
+        "growth": {
+            "inital": 13,
+            "regrow":4
+        },
+        "sells" : {
+            "base":50,
+            "silver": 62,
+            "gold": 75, 
+            "iridium":100
+        }
+    },
+    "corn": {
+        "seeds": {
+            "pierre":150,
+            "joja":187,
+            "special":0
+        },
+        "growth": {
+            "inital": 14,
+            "regrow":4
+        },
+        "sells" : {
+            "base":50,
+            "silver": 62,
+            "gold": 75, 
+            "iridium":100
+        }
+    },
+    "hops": {
+        "seeds": {
+            "pierre":60,
+            "joja":75,
+            "special":0
+        },
+        "growth": {
+            "inital": 11,
+            "regrow":1
+        },
+        "sells" : {
+            "base":25,
+            "silver": 31,
+            "gold": 37, 
+            "iridium":50
+        }
+    },
+    "hotPepper": {
+        "seeds": {
+            "pierre":40,
+            "joja":50,
+            "special":0
+        },
+        "growth": {
+            "inital": 5,
+            "regrow":3
+        },
+        "sells" : {
+            "base":40,
+            "silver": 50,
+            "gold": 60, 
+            "iridium": 80
+        }
+    },
+    "melon": {
+        "seeds": {
+            "pierre":80,
+            "joja":100,
+            "special":0
+        },
+        "growth": {
+            "inital": 12,
+            "regrow":0
+        },
+        "sells" : {
+            "base":250,
+            "silver": 312,
+            "gold": 375, 
+            "iridium":500
+        }
+    },
+    "poppy": {
+        "seeds": {
+            "pierre":100,
+            "joja":125,
+            "special":0
+        },
+        "growth": {
+            "inital": 7,
+            "regrow":0
+        },
+        "sells" : {
+            "base":140,
+            "silver": 175,
+            "gold": 210, 
+            "iridium":280
+        }
+    },
+    "radish": {
+        "seeds": {
+            "pierre":40,
+            "joja":50,
+            "special":0
+        },
+        "growth": {
+            "inital": 6,
+            "regrow":0
+        },
+        "sells" : {
+            "base":90,
+            "silver": 112,
+            "gold": 135, 
+            "iridium":180
+        }
+    },
+    "redCabbage": {
+        "seeds": {
+            "pierre":100,
+            "joja":0,
+            "special":0
+        },
+        "growth": {
+            "inital": 9,
+            "regrow":0
+        },
+        "sells" : {
+            "base":260,
+            "silver": 325,
+            "gold": 390, 
+            "iridium": 520
+        }
+    },
+    "starfruit": {
+        "seeds": {
+            "pierre":0,
+            "joja":0,
+            "special": 400
+        },
+        "growth": {
+            "inital": 13,
+            "regrow":0
+        },
+        "sells" : {
+            "base":750,
+            "silver": 937,
+            "gold": 1125, 
+            "iridium":1500 
+        }
+    },
+    "starfruit": {
+        "seeds": {
+            "pierre":0,
+            "joja":0,
+            "special": 400
+        },
+        "growth": {
+            "inital": 14,
+            "regrow":4
+        },
+        "sells" : {
+            "base":50,
+            "silver": 62,
+            "gold": 75, 
+            "iridium":100
+        }
+    },
+    "summerSpangle": {
+        "seeds": {
+            "pierre":50,
+            "joja":62,
+            "special": 0
+        },
+        "growth": {
+            "inital": 8,
+            "regrow": 0
+        },
+        "sells" : {
+            "base":90,
+            "silver": 112,
+            "gold": 135, 
+            "iridium":180
+        }
+    },
+    "sunflower": {
+        "seeds": {
+            "pierre":200,
+            "joja":125,
+            "special": 0
+        },
+        "growth": {
+            "inital": 8,
+            "regrow": 0
+        },
+        "sells" : {
+            "base":80,
+            "silver": 100,
+            "gold": 120, 
+            "iridium":160
+        }
+    },
+    "tomato": {
+        "seeds": {
+            "pierre":50,
+            "joja":62,
+            "special": 0
+        },
+        "growth": {
+            "inital": 4,
+            "regrow": 0
+        },
+        "sells" : {
+            "base":60,
+            "silver": 75,
+            "gold": 90, 
+            "iridium":120
+        }
+    },
+    "wheat": {
+        "seeds": {
+            "pierre":10,
+            "joja":12,
+            "special": 0
+        },
+        "growth": {
+            "inital": 4,
+            "regrow": 0
+        },
+        "sells" : {
+            "base":25,
+            "silver": 31,
+            "gold": 37, 
+            "iridium":50
+        }
+    },
+    "artichoke": {
+        "seeds": {
+            "pierre":30,
+            "joja":0,
+            "special": 0
+        },
+        "growth": {
+            "inital": 8,
+            "regrow": 0
+        },
+        "sells" : {
+            "base":160,
+            "silver": 200,
+            "gold": 240, 
+            "iridium": 320
+        }
+    },
+    "beet": {
+        "seeds": {
+            "pierre":0,
+            "joja":0,
+            "special": 20
+        },
+        "growth": {
+            "inital": 6,
+            "regrow": 0
+        },
+        "sells" : {
+            "base":100,
+            "silver": 125,
+            "gold": 150, 
+            "iridium":200
+        }
+    },
+    "bokChoy": {
+        "seeds": {
+            "pierre":50,
+            "joja":62,
+            "special": 0
+        },
+        "growth": {
+            "inital": 4,
+            "regrow": 0
+        },
+        "sells" : {
+            "base":80,
+            "silver": 100,
+            "gold": 120, 
+            "iridium":160
+        }
+    },
+    "cranberries": {
+        "seeds": {
+            "pierre":240,
+            "joja":300,
+            "special": 0
+        },
+        "growth": {
+            "inital": 7,
+            "regrow": 5
+        },
+        "sells" : {
+            "base":75,
+            "silver": 93,
+            "gold": 112, 
+            "iridium": 150
+        }
+    },
+    "eggplant": {
+        "seeds": {
+            "pierre":20,
+            "joja":25,
+            "special": 0
+        },
+        "growth": {
+            "inital": 5,
+            "regrow": 0
+        },
+        "sells" : {
+            "base":60,
+            "silver": 75,
+            "gold": 90, 
+            "iridium":120
+        }
+    },
+    "fairyRose": {
+        "seeds": {
+            "pierre":200,
+            "joja":250,
+            "special": 0
+        },
+        "growth": {
+            "inital": 12,
+            "regrow": 0
+        },
+        "sells" : {
+            "base":290,
+            "silver": 362,
+            "gold": 435, 
+            "iridium":580
+        }
+    },
+    "grape": {
+        "seeds": {
+            "pierre":60,
+            "joja":75,
+            "special": 0
+        },
+        "growth": {
+            "inital": 10,
+            "regrow": 3
+        },
+        "sells" : {
+            "base":80,
+            "silver": 100,
+            "gold": 120, 
+            "iridium":160
+        }
+    },
+    "summerSpangle": {
+        "seeds": {
+            "pierre":100,
+            "joja":125,
+            "special": 0
+        },
+        "growth": {
+            "inital": 13,
+            "regrow": 0
+        },
+        "sells" : {
+            "base":320,
+            "silver": 400,
+            "gold": 480, 
+            "iridium":640
+        }
+    },
+    "yam": {
+        "seeds": {
+            "pierre":60,
+            "joja":75,
+            "special": 0
+        },
+        "growth": {
+            "inital": 10,
+            "regrow": 0
+        },
+        "sells" : {
+            "base":160,
+            "silver": 200,
+            "gold": 240, 
+            "iridium":320
+        }
+    },
+};
